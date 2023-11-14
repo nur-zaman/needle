6393b9c2d03e636fa648e394fc557eeffd6e3f4f
NUR UZ ZAMAN <mail.nuruzzaman@gmail.com>
2023-11-14 22:00:53 +0600

    feat: added different code for data in attendance_data_config

--- attendance_data_config.py
+++ attendance_data_config.py
@@ -1,4 +1,4 @@
 class AttendanceData:
-    data = "15"
+    data = "11"  # 10 work from office ,11 Day Started, 12 took a break, 13 started to work, 14 day ended, 15 Started Again,16 worked from office,
     lat = "23.7968856"
     lng = "90.4219536"
